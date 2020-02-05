b30afc79ea62b86f966c524fc26cb9d602d798b9
Neha Kumari <32158383+neha39@users.noreply.github.com>
2020-02-05 15:46:48 +0530

    added kuratsuba multiplication algorithm

---
+++ Karatsuba Multiplication Algorithm.py
@@ -0,0 +1,32 @@
+def karatsuba_multiplication(num1,num2):
+    """
+    1. num1 and num2 are two numbers to be multiplied
+    2. the function returns the result after multiplication
+    """
+    if num1<10 or num2<10:
+        return num1*num2
+    else:
+        n=max(len(str(num1)),len(str(num2)))
+        nby2=n//2
+        
+        a=num1//(10**(nby2))
+        b=num1%(10**(nby2))
+        c=num2//(10**(nby2))
+        d=num2%(10**(nby2))
+        
+        ac=karatsuba_multiplication(a,c)
+        bd=karatsuba_multiplication(b,d)
+        ad_bc=karatsuba_multiplication(a+b,c+d)-ac-bd
+        
+        return ((10**(2*nby2))*ac)+((10**nby2)*ad_bc)+bd
+    
+""" 
+EXAMPLE TO TRY:
+
+num1=3141592653589793238462643383279502884197169399375105820974944592
+num2=2718281828459045235360287471352662497757247093699959574966967627
+
+print(karatsuba_multiplication(num1,num2))
+"""
+
+
